e2e40af1ce6f73d61f3255f5cb534e02525c9688
1273611319@qq.com <1273611319@qq.com>
2023-03-26 21:16:30 +0800

    项目工程化配置：eslint、stylelint及prettier相关配置

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { mock } from 'mockjs';
 
 type Data = {
   name: string
@@ -9,5 +10,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(mock({
+    'list|1-10': [{
+      // 属性 id 是一个自增数，起始值为 1，每次增 1
+      'id|+1': 1
+    }]
+  }))
 }
