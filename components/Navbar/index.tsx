e11bafe6130df6e5ab638dce1979a3df429f8c4d
1273611319@qq.com <1273611319@qq.com>
2023-03-28 20:21:38 +0800

    Navbar样式调整

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import navConfig from "./config";
 import styles from "./index.module.scss";
 
 const Navbar: NextPage = () => {
+  const { pathname } = useRouter();
   return (
     <div className={styles.navbar}>
       <section className={styles.logoArea}>BLOG-C</section>
       <section className={styles.linkArea}>
         {navConfig.map((nav) => (
           <Link key={nav.value} href={nav.value}>
-            <a>{nav.label}</a>
+            <a className={nav.value === pathname ? styles.active : ""}>
+              {nav.label}
+            </a>
           </Link>
         ))}
       </section>
